9c733440e1a0aa4606284b9656f643511042abdb
Adrien Samson <adrien@adriensamson.fr>
2024-12-01 19:00:48 +0100

    DMA IRQ

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ use rp_pico::hal::uart::{Reader, UartConfig, UartDevice, UartPeripheral, ValidUa
 use rp_pico::pac::{UART0};
 use rp_pico::pac::interrupt;
 use rp_pico::{entry, XOSC_CRYSTAL_FREQ};
-use rp_pico::hal::dma::{Channel, ChannelIndex, DMAExt, ReadTarget, CH0};
+use rp_pico::hal::dma::{Channel, ChannelIndex, DMAExt, ReadTarget, SingleChannel, CH0};
 use rp_pico::hal::dma::single_buffer::{Config, Transfer};
 use aoc_pico::{borrow, give, give_away_cell, shared_cell, take};
 use aoc_pico::aoc::AocRunner;
@@ -33,7 +33,7 @@ type UartPinout = (Pin<Gpio0, FunctionUart, PullDown>, Pin<Gpio1, FunctionUart, 
 
 give_away_cell!(UART_RX: Reader<UART0, UartPinout>);
 give_away_cell!(CONSOLE: Console);
-shared_cell!(OUT_DATA: VecDeque<Vec<u8>>);
+shared_cell!(OUT_DATA: OutQueue);
 give_away_cell!(CONSOLE_WRITER: ConsoleUartDmaWriter<CH0, UART0, UartPinout>);
 
 #[entry]
@@ -42,6 +42,7 @@ fn entry() -> ! {
 
     unsafe {
         NVIC::unmask(interrupt::UART0_IRQ);
+        NVIC::unmask(interrupt::DMA_IRQ_0);
     }
 
     idle()
@@ -91,25 +92,21 @@ fn init() {
     let console = Console::new(commands);
     uart_rx.enable_rx_interrupt();
 
-    let dma_chans = pac.DMA.split(&mut pac.RESETS);
+    let mut dma_chans = pac.DMA.split(&mut pac.RESETS);
+    dma_chans.ch0.enable_irq0();
     let console_writer = ConsoleUartDmaWriter::Ready(uart_tx, dma_chans.ch0);
 
     give!(UART_RX = uart_rx);
     give!(CONSOLE = console);
     give!(CONSOLE_WRITER = console_writer);
-    give!(OUT_DATA = VecDeque::new());
+    give!(OUT_DATA = OutQueue::new());
 }
 
 fn idle() -> ! {
     debug!("stack pointer: {:x}", read_sp());
 
-    let console_writer = take!(CONSOLE_WRITER);
     loop {
-        if let Some(data) = borrow!(OUT_DATA, |vec| vec.pop_front()) {
-            console_writer.output(data);
-        } else {
-            cortex_m::asm::wfi()
-        }
+        cortex_m::asm::wfi()
     }
 }
 
@@ -124,8 +121,22 @@ fn UART0_IRQ() {
     while let Some(vec) = read_into_vec(uart_rx, CHUNK_SIZE) {
         console.push(vec);
         for out in &mut *console {
-            borrow!(OUT_DATA, |vec| vec.push_back(out));
+            let need_pend = borrow!(OUT_DATA, |queue| queue.push(out));
+            if need_pend {
+                NVIC::pend(interrupt::DMA_IRQ_0);
+            }
         }
+    }
+}
+
+#[interrupt]
+fn DMA_IRQ_0() {
+    let console_writer = take!(CONSOLE_WRITER);
+    console_writer.check_irq0();
+    if let Some(data) = borrow!(OUT_DATA, |vec| vec.pop()) {
+        console_writer.output(data);
+    } else {
+        console_writer.flush();
     }
 }
 
@@ -136,6 +147,23 @@ fn read_into_vec<D: UartDevice, P: ValidUartPinout<D>>(uart: &Reader<D, P>, max_
     let len = uart.read_raw(buf).ok()?;
     unsafe { vec.set_len(vec.len() + len) };
     Some(vec)
+}
+
+struct OutQueue(VecDeque<Vec<u8>>);
+
+impl OutQueue {
+    fn new() -> Self {
+        Self(VecDeque::new())
+    }
+
+    fn push(&mut self, data: Vec<u8>) -> bool {
+        self.0.push_back(data);
+        self.0.len() == 1
+    }
+
+    fn pop(&mut self) -> Option<Vec<u8>> {
+        self.0.pop_front()
+    }
 }
 
 struct VecReadTarget(Vec<u8>);
@@ -174,6 +202,25 @@ impl <D: ChannelIndex, U: UartDevice, P: ValidUartPinout<U>> ConsoleUartDmaWrite
                 self.output(line);
             },
             Self::Poisoned => unreachable!(),
+        }
+    }
+
+    fn flush(&mut self) {
+        match core::mem::replace(self, Self::Poisoned) {
+            Self::Ready(writer, ch) => *self = Self::Ready(writer, ch),
+            Self::Transferring(transfer) => {
+                let (ch, _, writer) = transfer.wait();
+                *self = Self::Ready(writer, ch);
+            },
+            Self::Poisoned => unreachable!(),
+        }
+    }
+
+    fn check_irq0(&mut self) -> bool {
+        match self {
+            Self::Ready(_, ch) => ch.check_irq0(),
+            Self::Transferring(transfer) => transfer.check_irq0(),
+            Self::Poisoned => false,
         }
     }
 }
