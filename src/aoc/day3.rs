fe265a21f73308a4939fc121d2f51d62ea0417aa
Adrien Samson <adrien@adriensamson.fr>
2024-12-04 19:50:34 +0100

    day 3 part 2

--- src/aoc/day3.rs
+++ src/aoc/day3.rs
@@ -7,7 +7,7 @@ pub struct AocDay3 {
 }
 
 impl AocDay for AocDay3 {
-    fn new(mut input: Vec<String>) -> Self {
+    fn new(input: Vec<String>) -> Self {
         Self {
             code: input.join("\n"),
         }
@@ -16,6 +16,32 @@ impl AocDay for AocDay3 {
     fn part1(&self) -> String {
         let sum: u32 = (0..self.code.len()-8)
             .filter_map(|i| {
+                let s = self.code[i..].strip_prefix("mul(")?;
+                let comma = s[..s.len().min(4)].find(',')?;
+                let x : u32 = s[..comma].parse().ok()?;
+                let s = &s[comma+1..];
+                let paren = s[..s.len().min(4)].find(')')?;
+                let y : u32 = s[..paren].parse().ok()?;
+                Some(x * y)
+            }).sum();
+        sum.to_string()
+    }
+
+    fn part2(&self) -> String {
+        let mut enabled = true;
+        let sum: u32 = (0..self.code.len()-8)
+            .filter_map(|i| {
+                if self.code[i..].starts_with("do()") {
+                    enabled = true;
+                    return None;
+                }
+                if self.code[i..].starts_with("don't()") {
+                    enabled = false;
+                    return None;
+                }
+                if !enabled {
+                    return None;
+                }
                 let s = self.code[i..].strip_prefix("mul(")?;
                 let comma = s[..s.len().min(4)].find(',')?;
                 let x : u32 = s[..comma].parse().ok()?;
@@ -35,10 +61,13 @@ mod test {
     use crate::aoc::AocDay;
 
     const INPUT : &'static str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const INPUT2 : &'static str = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn test() {
-        let day = AocDay3::new(vec![INPUT.into()]);
-        assert_eq!(day.part1(), "161");
+        let part1 = AocDay3::new(vec![INPUT.into()]);
+        assert_eq!(part1.part1(), "161");
+        let part2 = AocDay3::new(vec![INPUT2.into()]);
+        assert_eq!(part2.part2(), "48");
     }
 }
