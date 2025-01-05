3dc3bd9e86b518f5152cfd201fe5a0b46dc0cfbc
Adrien Samson <adrien@adriensamson.fr>
2025-01-05 17:02:08 +0100

    alarm 1 to 3

--- rp2040-async/src/lib.rs
+++ rp2040-async/src/lib.rs
@@ -10,6 +10,6 @@ pub use dma::DmaIrq0Handler;
 pub use dma::DmaIrq1Handler;
 use rp2040_hal::pac::UART0;
 //use rp2040_hal::uart::ValidUartPinout;
-pub use timer::{TimerIrq0Handler, AsyncAlarm};
+pub use timer::{TimerIrqHandler, AsyncAlarm};
 pub use uart::UartIrqHandler;
 pub type Uart0IrqHandler<P /*: ValidUartPinout<UART0>*/> = UartIrqHandler<UART0, P>;
