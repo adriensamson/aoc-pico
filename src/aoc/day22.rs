a96818416a503c013cfb1b1c5b2079613736e6f6
Adrien Samson <adrien@adriensamson.fr>
2025-10-29 17:52:54 +0100

    day 22 part 1

--- src/aoc/day22.rs
+++ src/aoc/day22.rs
@@ -1,11 +1,34 @@
+use alloc::format;
 use alloc::string::String;
 use alloc::vec::Vec;
+use defmt::debug;
 use crate::aoc::AocDay;
 
-pub struct AocDay22;
+pub struct AocDay22 {
+    secrets: Vec<u32>,
+}
 
 impl AocDay for AocDay22 {
-    fn new(_input: Vec<String>) -> Self {
-        Self
+    fn new(input: Vec<String>) -> Self {
+        Self {
+            secrets: input.iter().filter_map(|s| s.parse().ok()).collect(),
+        }
     }
+
+    fn part1(&self) -> String {
+        let sum: u64 = self.secrets.iter().copied().map(|mut secret| {
+            for _ in 0..2000 {
+                secret = next_secret(secret);
+            }
+            secret as u64
+        }).sum();
+        format!("{sum}")
+    }
+}
+
+fn next_secret(mut secret: u32) -> u32 {
+    secret = ((secret << 6) ^ secret) % 0x100_0000;
+    secret = ((secret >> 5) ^ secret) % 0x100_0000;
+    secret = ((secret << 11) ^ secret) % 0x100_0000;
+    secret
 }
