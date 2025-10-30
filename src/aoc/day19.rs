dce68cc1d469e0d2fdace2895765d4056bc560ea
Adrien Samson <adrien@adriensamson.fr>
2025-10-30 18:41:58 +0100

    rewrite and fix day 19 part 1

--- src/aoc/day19.rs
+++ src/aoc/day19.rs
@@ -2,19 +2,23 @@ use alloc::{format, vec};
 use alloc::collections::BTreeSet;
 use alloc::string::{String, ToString};
 use alloc::vec::Vec;
+use core::ops::Bound;
 use crate::aoc::AocDay;
 
 pub struct AocDay19 {
-    available_towels: Vec<String>,
+    available_towels: BTreeSet<String>,
     patterns: Vec<String>,
 }
 
 impl AocDay for AocDay19 {
     fn new(input: Vec<String>) -> Self {
         let mut iter = input.into_iter();
-        let available_towels = iter.next().unwrap().split(", ").map(ToString::to_string).collect();
+        let available_towels = iter.next().unwrap()
+            .split(", ")
+            .map(ToString::to_string)
+            .collect();
         iter.next();
-        let patterns = iter.collect();
+        let patterns = iter.filter(|s| !s.is_empty()).collect();
         Self {
             available_towels,
             patterns,
@@ -22,31 +26,31 @@ impl AocDay for AocDay19 {
     }
 
     fn part1(&self) -> String {
-        let available_set : BTreeSet<&str> = self.available_towels.iter().map(String::as_str).collect();
-        let max_len = available_set.iter().map(|s| s.len()).max().unwrap();
-        let n = self.patterns.iter().filter(|pattern| {
-            let pattern = pattern.as_str();
-            let mut backtrack = vec![(0, max_len+1)];
-            let mut impossible_ends : BTreeSet<usize> = BTreeSet::new();
-            while let Some((start, prev_len)) = backtrack.pop() {
-                let rest = &pattern[start..];
-                if impossible_ends.contains(&rest.len()) {
-                    continue;
-                }
-                for i in (1..prev_len.min(rest.len() + 1)).rev() {
-                    if available_set.contains(&rest[0..i]) {
-                        backtrack.push((start, i));
-                        if rest[i..].is_empty() {
-                            return true;
-                        }
-                        backtrack.push((start + i, max_len+1));
-                        break;
-                    }
-                }
-                impossible_ends.insert(rest.len());
-            }
-            false
-        }).count();
-        format!("{n}")
+        let count = self.patterns.iter().filter(|s| is_doable(s, &self.available_towels)).count();
+
+        format!("{count}")
     }
+}
+
+fn is_doable(patt: &str, available_towels: &BTreeSet<String>) -> bool {
+    let mut to_check = vec![patt];
+    let mut failed_ends = BTreeSet::new();
+    while let Some(pattern) = to_check.pop() {
+        for towel in available_towels.range::<str, _>((Bound::Included(&pattern[..1]), Bound::Included(pattern))) {
+            let mut found = false;
+            if let Some(next) = pattern.strip_prefix(towel) {
+                found = true;
+                if next.is_empty() {
+                    return true;
+                }
+                if !failed_ends.contains(&next) {
+                    to_check.push(next);
+                }
+            }
+            if !found {
+                failed_ends.insert(pattern);
+            }
+        }
+    }
+    false
 }
