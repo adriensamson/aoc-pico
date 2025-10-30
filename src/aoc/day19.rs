d0b6507c20b2c71eefae9bf4e94be1764f0bf6da
Adrien Samson <adrien@adriensamson.fr>
2025-10-30 19:26:07 +0100

    day 19 part 2

--- src/aoc/day19.rs
+++ src/aoc/day19.rs
@@ -1,7 +1,8 @@
 use alloc::{format, vec};
-use alloc::collections::BTreeSet;
+use alloc::collections::{BTreeMap, BTreeSet};
 use alloc::string::{String, ToString};
 use alloc::vec::Vec;
+use core::cmp::Ordering;
 use core::ops::Bound;
 use crate::aoc::AocDay;
 
@@ -27,8 +28,28 @@ impl AocDay for AocDay19 {
 
     fn part1(&self) -> String {
         let count = self.patterns.iter().filter(|s| is_doable(s, &self.available_towels)).count();
-
         format!("{count}")
+    }
+
+    fn part2(&self) -> String {
+        let count : u64 = self.patterns.iter().map(|s| num_ways(s, &self.available_towels)).sum();
+        format!("{count}")
+    }
+}
+
+struct StartsWith<'a>(&'a str);
+
+impl core::ops::RangeBounds<str> for StartsWith<'_> {
+    fn start_bound(&self) -> Bound<&str> {
+        if self.0.is_empty() {
+            Bound::Included(self.0)
+        } else {
+            Bound::Included(&self.0[..1])
+        }
+    }
+
+    fn end_bound(&self) -> Bound<&str> {
+        Bound::Included(self.0)
     }
 }
 
@@ -36,7 +57,7 @@ fn is_doable(patt: &str, available_towels: &BTreeSet<String>) -> bool {
     let mut to_check = vec![patt];
     let mut failed_ends = BTreeSet::new();
     while let Some(pattern) = to_check.pop() {
-        for towel in available_towels.range::<str, _>((Bound::Included(&pattern[..1]), Bound::Included(pattern))) {
+        for towel in available_towels.range(StartsWith(pattern)) {
             let mut found = false;
             if let Some(next) = pattern.strip_prefix(towel) {
                 found = true;
@@ -53,4 +74,39 @@ fn is_doable(patt: &str, available_towels: &BTreeSet<String>) -> bool {
         }
     }
     false
+}
+
+#[derive(Eq, PartialEq)]
+struct BySize<'a>(&'a str);
+
+impl Ord for BySize<'_> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.len().cmp(&other.0.len()).then(self.0.cmp(&other.0))
+    }
+}
+impl PartialOrd for BySize<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn num_ways(patt: &str, available_towels: &BTreeSet<String>) -> u64 {
+    let mut to_check = BTreeSet::new();
+    to_check.insert(BySize(patt));
+    let mut end_ways : BTreeMap<&str, u64> = BTreeMap::new();
+    end_ways.insert("", 1);
+    while let Some(BySize(pattern)) = to_check.pop_first() {
+        let towels : Vec<_> = available_towels.range(StartsWith(pattern))
+            .filter_map(|t| pattern.strip_prefix(t.as_str()))
+            .collect();
+        let missing : Vec<_> = towels.iter().copied().filter(|next| !end_ways.contains_key(next)).collect();
+        if !missing.is_empty() {
+            to_check.insert(BySize(pattern));
+            to_check.extend(missing.into_iter().map(BySize));
+        } else {
+            let count : u64 = towels.iter().map(|next| end_ways.get(next).unwrap()).sum();
+            end_ways.insert(pattern, count);
+        }
+    }
+    end_ways.get(patt).copied().unwrap()
 }
